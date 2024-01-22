38d5f3de6616fc1db33a5e4caa7f7745c2c32a2c
Leon Liang <ycliang.cn@outlook.com>
2024-01-22 23:52:49 +0000

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
-import { title } from "@/components/primitives";
 import React from "react";
 
 import { Metadata } from 'next';
+import { Avatar, Badge } from "@nextui-org/react";
 export const metadata: Metadata = {
 	title: '关于我',
 	description: '我是谁？',
@@ -14,8 +14,17 @@ export default function AboutPage() {
 				<h1 className={"section-title"}>关于我</h1>
 				<h2 className={"section-subtitle"}>随便写点简介吧</h2>
 			</div>
-			<div>
-
+			<div className={"intro flex sm:grid grid-cols-6 gap-4"}>
+				<div className={"col-span-4 border-2"}>你好</div>
+				<div className={"col-span-2 border-2 p-4"}>
+					<Badge color="success" shape="circle" placement="bottom-right" className={"w-8 h-8"}>
+						<Avatar
+							className={"w-24 h-24"}
+							src="/assets/avatar.jpg"
+							radius={"md"}
+						/>
+					</Badge>
+				</div>
 			</div>
 		</>
 
