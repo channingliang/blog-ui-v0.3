c6becf12622c446f3e1cfb0025790f40f2e04b4f
Leon Liang <ycliang.cn@outlook.com>
2024-01-24 23:23:51 +0000

    update about page with map

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,18 @@
-'use client';
-
 import React from "react";
 
 import { Metadata } from 'next';
 import { Avatar, Badge } from "@nextui-org/react";
 import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+const Map =
+	dynamic(() => import("@/components/about/location"), {
+		ssr: false,
+	});
 
-// export const metadata: Metadata = {
-// 	title: '关于我',
-// 	description: '我是谁？',
-// };
+export const metadata: Metadata = {
+	title: '关于我',
+	description: '我是谁？',
+};
 
 export default function AboutPage() {
 	return (
@@ -19,7 +22,7 @@ export default function AboutPage() {
 				<h2 className={"section-subtitle"}>随便写点简介吧</h2>
 			</div>
 			<div className={"intro"}>
-				<div className={"col-span-6 sm:flex items-center gap-8 border-none"}>
+				<div className={"col-span-8 sm:flex items-center gap-8 border-none"}>
 					<Avatar
 						className={"mb-8 sm:mb-0 mx-auto flex-shrink-0 w-20 h-20"}
 						isBordered
@@ -27,38 +30,49 @@ export default function AboutPage() {
 						src="/assets/avatar.jpg"
 						radius={"md"}
 					/>
-					<div className={"flex-grow p-4 px-8 rounded-xl border-2 h-full"}>
+					<div className={"flex-grow py-4 px-8 rounded-xl border-2 h-full"}>
 						<p>Hello~很高兴在这里见到你❤️</p>
 						<p className={"text-[2.5rem] font-bold"}>我叫 LEON</p>
-						<p className={"text-lg"}>是是一名普普通通的学生、公路车铁驭、超级大吃货、未来可能的全栈工程师</p>
+						<p className={"text-lg"}>是一名普通的大学牲、公路车铁驭、超级大吃货、未来的全栈工程师</p>
 					</div>
 				</div>
-				<div className={"col-span-6"}>
-					<div className="box mb-4 pt-4 pb-6 rounded text-center overflow-hidden h-[200px]">
-						<div className="lean-box flex">
-							<div className="wrapper mt-[-80px] flex flex-nowrap animate-row-up">
-								{Array.from({length: 4}, (_, i) => (
-									<div key={i} className="flex flex-col">
-										<div
-											className="text-[55px] leading-none tracking-[10px] w-[300px] text-white font-bold">Hello~
-										</div>
-										<div
-											className="text-[55px] leading-none tracking-[10px] w-[300px] text-white font-bold mt-1 mb-1"> (｡･∀･)ﾉﾞ
-										</div>
-									</div>
-								))}
-							</div>
-						</div>
+				<div className={"col-span-8 py-4 px-8 text-center font-bold bg-foreground"}>
+					<p className={"text-[5rem] md:text-[8vw] italic text-background leading-tight"}>
+						HELLO WORLD
+					</p>
+				</div>
+				<div className={"col-span-4 relative"}>
+					<Map latitude={55.94533} longitude={-3.18711} />
+					<div className="absolute bottom-0 left-0 z-10 flex items-center
+					 w-full p-4 bg-foreground/70">
+						<span className={"text-lg text-background"}>
+							正在探索区域
+							<span className={"text-xl font-bold"}> Edinburgh, United Kingdom</span>
+						</span>
 					</div>
 				</div>
-				<div className={"col-span-4"}>
-
+				<div className={"col-span-4 p-4"}>
+					<p>年龄：23</p>
+					<p>就读于爱丁堡大学</p>
 				</div>
-				<div className={"col-span-2"}>
-
+				<div className={"col-span-6 p-4"}>
+					<p>STEAM</p>
 				</div>
-
-
+				<div className={"col-span-2 p-4"}>
+					<p>最喜欢的游戏</p>
+				</div>
+				<div className={"col-span-3 p-4"}>
+					<p>技术栈</p>
+				</div>
+				<div className={"col-span-5 p-4"}>
+					<p>博客更新日志</p>
+				</div>
+				<div className={"col-span-5 p-4"}>
+					<p>近期动态</p>
+				</div>
+				<div className={"col-span-3 p-4"}>
+					<p>性格测试</p>
+				</div>
 			</div>
 
 
---
+++ components/about/location.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// @ts-ignore
+const customIcon = new L.divIcon({
+    className: 'location-icon',
+    iconSize: [20, 20]  // 调整为与 CSS 中定义的大小相匹配
+});
+
+type MapProps = {
+    latitude: number;
+    longitude: number;
+};
+
+const MapComponent: React.FC<MapProps> = ({ latitude, longitude }) => {
+    return (
+        <MapContainer className={"z-0"} center={[latitude, longitude]} zoom={13} style={{ height: '250px', width: '100%' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[latitude, longitude]} icon={customIcon} />
+        </MapContainer>
+    );
+};
+
+export default MapComponent;
