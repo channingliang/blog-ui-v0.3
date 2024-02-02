9ced1d313b1d38a8ff4a5d6f25ff3abdfd68b80f
Leon Liang <ycliang.cn@outlook.com>
2024-02-02 22:19:43 +0000

    update for deploy

--- next.config.js
+++ next.config.js
@@ -34,10 +34,6 @@ const nextConfig = {
     ],
   },
   output: "standalone",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
