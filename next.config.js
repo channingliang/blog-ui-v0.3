74dd808606e2f930063a0f0d3ed2246bb4707b7a
Leon Liang <ycliang.cn@outlook.com>
2024-02-02 22:15:17 +0000

    ignore lint error when build

--- next.config.js
+++ next.config.js
@@ -34,6 +34,10 @@ const nextConfig = {
     ],
   },
   output: "standalone",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
